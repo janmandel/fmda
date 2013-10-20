ffbeff596132d3520db5c3f6daac32f2c70d61b7
Martin Vejmelka <vejmelkam@gmail.com>
2013-10-20 10:37:30 -0600

    fixed version with correct diagnostic output

--- src/fmda.py
+++ src/fmda.py
@@ -8,7 +8,6 @@ Created on Sun Oct 28 18:14:36 2012
 from kriging_methods import trend_surface_model_kriging
 from wrf_model_data import WRFModelData
 from grid_moisture_model import GridMoistureModel
-from cell_model_opt import CellMoistureModel
 from observation_stations import MesoWestStation
 from diagnostics import init_diagnostics, diagnostics
 from spatial_model_utilities import great_circle_distance
@@ -206,10 +205,6 @@ def run_module():
     mresV = np.zeros_like(E)
     mid = np.zeros_like(E)
     Kg = np.zeros((dom_shape[0], dom_shape[1], len(Tk)+2))
-    cf = np.zeros((dom_shape[0], dom_shape[1], Nk+2))
-    cf_na = np.zeros((dom_shape[0], dom_shape[1], Nk+2))
-    f = np.zeros((dom_shape[0], dom_shape[1], Nk+2))
-    f_na = np.zeros((dom_shape[0], dom_shape[1], Nk+2))
 
     # preprocess all static covariates
     cov_ids = cfg['covariates']
@@ -235,13 +230,6 @@ def run_module():
     # retrieve assimilation time window
     assim_time_win = cfg['assimilation_time_window']
 
-    # construct model grid using standard fuel parameters
-    cmodels = np.zeros(dom_shape, dtype=np.object)
-    cmodels_na = np.zeros(dom_shape, dtype=np.object)
-    for i,j in np.ndindex(dom_shape):
-      cmodels[i,j] = CellMoistureModel((lat[i,j],lon[i,j]),Nk,E[i,j],Tk,P0)
-      cmodels_na[i,j] = CellMoistureModel((lat[i,j],lon[i,j]),Nk,E[i,j],Tk,P0)
-
     models = GridMoistureModel(E[:,:,np.newaxis][:,:,np.zeros((4,),dtype=np.int)], Tk, P0)
     models_na = GridMoistureModel(E[:,:,np.newaxis][:,:,np.zeros((4,),dtype=np.int)], Tk, P0)
 
@@ -263,50 +251,19 @@ def run_module():
         print("INFO: time: %s, step: %d" % (str(model_time), t))
 
         diagnostics().push("mt", model_time)
-        
-        # for i,j in np.ndindex(dom_shape):
-        #   cf[i,j,:] = cmodels[i,j].get_state()
-        #   cf_na[i,j,:] = cmodels_na[i,j].get_state()
 
-        # extract fuel moisture contents
-        f[:,:,:] = models.get_state()
-        f_na[:,:,:] = models_na.get_state()
-
-        # print("PRE update    ASSIM:   Max difference in state: %.18e" % (np.amax(np.fabs(cf - f))))
-        # print("PRE update NO ASSIM:   Max difference in state: %.18e" % (np.amax(np.fabs(cf_na - f_na))))
-        # print("PRE update    ASSIM:   Max diff. in Jacobian: %.18e" % (np.amax([np.amax(np.fabs(cmodels[p].get_J() - models.J[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-        # print("PRE update NO ASSIM:   Max diff. in Jacobian: %.18e" % (np.amax([np.amax(np.fabs(cmodels_na[p].get_J() - models_na.J[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-        # print("PRE update    ASSIM:   Max diff. in Covariance: %.18e" % (np.amax([np.amax(np.fabs(cmodels[p].get_state_covar() - models.P[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-        # print("PRE update NO ASSIM:   Max diff. in Covariance: %.18e" % (np.amax([np.amax(np.fabs(cmodels_na[p].get_state_covar() - models_na.P[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-
-        # # run the model update
-        # for i,j in np.ndindex(dom_shape):
-        #   cmodels_na[i,j].advance_model(Ed[t-1,i,j], Ew[t-1,i,j], rain[t-1,i,j], dt, Q)
-        #   cmodels[i,j].advance_model(Ed[t-1,i,j], Ew[t-1,i,j], rain[t-1,i,j], dt, Q)
-        #   cf[i,j,:] = cmodels[i,j].get_state()
-        #   cf_na[i,j,:] = cmodels_na[i,j].get_state()
 
         models_na.advance_model(Ed[t-1,:,:], Ew[t-1,:,:], rain[t-1,:,:], dt, Q)
         models.advance_model(Ed[t-1,:,:], Ew[t-1,:,:], rain[t-1,:,:], dt, Q)
 
-        # extract fuel moisture contents
-        f[:,:,:] = models.get_state()
-        f_na[:,:,:] = models_na.get_state()
-
-        # print("POST update    ASSIM:  Max difference in state at %s" % str(np.unravel_index(np.argmax(np.fabs(cf - f)), cf.shape)))
-        
-        # print("POST update    ASSIM:  Max difference in state: %.18e" % (np.amax(np.fabs(cf - f))))
-        # print("POST update NO ASSIM:  Max difference in state: %.18e" % (np.amax(np.fabs(cf_na - f_na))))
-
-        # print("POST update    ASSIM:  Max diff. in Jacobian: %.18e" % (np.amax([np.amax(np.fabs(cmodels[p].get_J() - models.J[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-        # print("POST update NO ASSIM:  Max diff. in Jacobian: %.18e" % (np.amax([np.amax(np.fabs(cmodels_na[p].get_J() - models_na.J[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-        # print("POST update    ASSIM:  Max diff. in Covariance: %.18e" % (np.amax([np.amax(np.fabs(cmodels[p].get_state_covar() - models.P[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
-        # print("POST update NO ASSIM:  Max diff. in Covariance: %.18e" % (np.amax([np.amax(np.fabs(cmodels_na[p].get_state_covar() - models_na.P[p[0],p[1],:,:])) for p in np.ndindex(dom_shape)])))
+        # extract fuel moisture contents [make a fresh copy every iteration!]
+        f = models.get_state().copy()
+        f_na = models_na.get_state().copy()
 
         # push 10-hr fuel state & variance of forecast
-        diagnostics().push("fm10f", f[:,:,1].copy())
-        diagnostics().push("fm10f_V1", models.P[:,:,1,1].copy())
-        diagnostics().push("fm10na", f_na[:,:,1].copy())
+        diagnostics().push("fm10f", models.get_state()[:,:,1].copy())
+        diagnostics().push("fm10f_V1", models.P[:,:,1,1])
+        diagnostics().push("fm10na", models_na.get_state()[:,:,1].copy())
 
         # run Kriging on each observed fuel type
         Kfs, Vfs, fns = [], [], []
@@ -347,12 +304,8 @@ def run_module():
 
                 mod_vals    = np.array([f[i,j,fuel_ndx] for i,j in obs_ngp])
                 mod_na_vals = np.array([f_na[i,j,fuel_ndx] for i,j  in obs_ngp])
-                cmod_vals    = np.array([cf[i,j,fuel_ndx] for i,j in obs_ngp])
-                cmod_na_vals = np.array([cf_na[i,j,fuel_ndx] for i,j  in obs_ngp])
                 diagnostics().push("fm10f_rmse", np.mean((obs_vals - mod_vals)**2)**0.5)
-                diagnostics().push("cfm10f_rmse", np.mean((obs_vals - cmod_vals)**2)**0.5)
                 diagnostics().push("fm10na_rmse", np.mean((obs_vals - mod_na_vals)**2)**0.5)
-                diagnostics().push("cfm10na_rmse", np.mean((obs_vals - cmod_na_vals)**2)**0.5)
 
                 # krige observations to grid points
                 Kf_fn, Vf_fn = trend_surface_model_kriging(obs_valid_now, X)
@@ -393,10 +346,6 @@ def run_module():
                 models.kalman_update_single(O, V, fns[0], Kg)
             else:
                 models.kalman_update(O, V, fns, Kg)
-
-            # for i,j in np.ndindex(dom_shape):
-            #     cmodels[i,j].kalman_update(O[i,j,:], V[i,j,:,:], fns)
-            #     cf[i,j,:] = cmodels[i,j].get_state()
 
             # push new diagnostic outputs
             diagnostics().push("K1", (t, Kg[:,:,1].copy()))
